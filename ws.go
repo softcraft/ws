aef382b7379cfa2dda51dd4a53bbb09795ed19a2
Javier Cervantes <1.27201@gmail.com>
2014-11-18 23:25:14 -0600

    new ws methods

--- ws.go
+++ ws.go
@@ -2,51 +2,46 @@ package ws
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
 	"net/http"
-	"net/url"
 	"os"
 )
 
-type Handler func(url.Values) (int, interface{})
-type Route struct {
-	Method  string
-	Path    string
-	Handler Handler
-}
+type handler func(w http.ResponseWriter, r *http.Request)
 
-func StartServer(routes []Route) {
-	rtr := mux.NewRouter()
-
-	for _, route := range routes {
-		rtr.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
-			r.ParseMultipartForm(0)
-
-			if r.FormValue("access_token") != os.Getenv("ACCESS_TOKEN") {
-				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(http.StatusUnauthorized)
-				authErr, _ := json.Marshal(map[string]string{"error": "Incorrect access token provided"})
-				w.Write(authErr)
-			} else {
-				status, response := route.Handler(r.Form)
-				jsonResponse, _ := json.Marshal(response)
-
-				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(status)
-				w.Write(jsonResponse)
-			}
-
-		}).Methods(route.Method)
+func Start(path string, handler handler) {
+	var port = ":" + os.Getenv("PORT")
+	if port == ":" {
+		port = ":8181"
 	}
 
-	http.Handle("/", rtr)
-	http.ListenAndServe(getPort(), nil)
+	http.HandleFunc(path, Authorize(handler))
+	http.ListenAndServe(port, nil)
 }
 
-func getPort() string {
-	var port = os.Getenv("PORT")
-	if port == "" {
-		port = "8181"
+func Success(w http.ResponseWriter, status int, response map[string]interface{}) {
+	jsonResponse, _ := json.Marshal(response)
+
+	w.WriteHeader(status)
+	w.Write(jsonResponse)
+}
+
+func Error(w http.ResponseWriter, status int, err string) {
+	response := map[string]string{"error": err}
+	jsonResponse, _ := json.Marshal(response)
+
+	w.WriteHeader(status)
+	w.Write(jsonResponse)
+}
+
+func Authorize(f handler) handler {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.FormValue("access_token") != os.Getenv("ACCESS_TOKEN") {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusUnauthorized)
+			authErr, _ := json.Marshal(map[string]string{"error": "Incorrect access token provided"})
+			w.Write(authErr)
+			return
+		}
+		f(w, r)
 	}
-	return ":" + port
 }
